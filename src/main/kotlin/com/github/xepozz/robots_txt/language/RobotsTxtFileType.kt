408994110e1a3cefafb6873efdbe3ca3a387156d
Dmitriy Derepko <xepozz@list.ru>
2025-02-10 11:44:31 +0300

    fix filename mismatch
    Signed-off-by: Dmitriy Derepko <xepozz@list.ru>

--- src/main/kotlin/com/github/xepozz/robots_txt/language/RobotsTxtFileType.kt
+++ src/main/kotlin/com/github/xepozz/robots_txt/language/RobotsTxtFileType.kt
@@ -4,7 +4,7 @@ import com.github.xepozz.robots_txt.RobotsTxtIcons
 import com.intellij.openapi.fileTypes.LanguageFileType
 
 object RobotsTxtFileType : LanguageFileType(RobotsTxtLanguage.INSTANCE) {
-    override fun getName() = "robots.txt File"
+    override fun getName() = "RobotsTxt File"
     override fun getDescription() = "robots.txt file for controlling web crawler access"
     override fun getDefaultExtension() = "txt"
     override fun getIcon() = RobotsTxtIcons.FILE
