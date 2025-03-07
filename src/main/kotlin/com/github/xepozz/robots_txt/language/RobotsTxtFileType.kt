6c83b4647a0d75d42bdfb1b44d3efc1de8be8bf7
Dmitriy Derepko <xepozz@list.ru>
2025-03-07 18:21:25 +0300

    tag 2025.0.11
    Signed-off-by: Dmitriy Derepko <xepozz@list.ru>

--- src/main/kotlin/com/github/xepozz/robots_txt/language/RobotsTxtFileType.kt
+++ src/main/kotlin/com/github/xepozz/robots_txt/language/RobotsTxtFileType.kt
@@ -3,9 +3,15 @@ package com.github.xepozz.robots_txt.language
 import com.github.xepozz.robots_txt.RobotsTxtIcons
 import com.intellij.openapi.fileTypes.LanguageFileType
 
-object RobotsTxtFileType : LanguageFileType(RobotsTxtLanguage.INSTANCE) {
+class RobotsTxtFileType private constructor() : LanguageFileType(RobotsTxtLanguage.INSTANCE) {
     override fun getName() = "RobotsTxt File"
     override fun getDescription() = "robots.txt file for controlling web crawler access"
     override fun getDefaultExtension() = "txt"
     override fun getIcon() = RobotsTxtIcons.FILE
+
+    companion object {
+        @Suppress("unused")
+        @JvmStatic
+        val INSTANCE = RobotsTxtFileType()
+    }
 }
